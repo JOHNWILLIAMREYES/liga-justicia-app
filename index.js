01a5d8142b86f8f7343f0ed6553d1652b0d147fb
JOHNWILLIAMREYES <johnwilliam_reyes@hotmail.com>
2024-01-03 22:14:30 -0500

    add pokeapi

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const axios = require('axios');
 
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
+});
+app.get('/pokeApi/:pokemon_id', (req, res) => {
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'https://pokeapi.co/api/v2/gender/1',
+    headers: {},
+  };
+
+  axios
+    .request(config)
+    .then((response) => {
+      res.send(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  //   res.send('¡Hola, mundo!');
 });
 
 app.listen(port, () => {
