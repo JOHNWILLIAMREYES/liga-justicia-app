095e971728791f53a03e0444094589eb7939cf81
JOHNWILLIAMREYES <johnwilliam_reyes@hotmail.com>
2024-01-03 21:51:47 -0500

    add hol

--- index.js
+++ index.js
@@ -1,62 +1,63 @@
-const express = require('express');
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors');
-const { PORT } = require('./app/config/appconfig');
-const routers = require('./app/api-routes');
-const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger_output.json');
-const path = require('path');
+// const express = require('express');
+// require('dotenv').config();
+// const bodyParser = require('body-parser');
+// const morgan = require('morgan');
+// const cors = require('cors');
+// const { PORT } = require('./app/config/appconfig');
+// const routers = require('./app/api-routes');
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerFile = require('./swagger_output.json');
+// const path = require('path');
 
-class SERVER {
-  constructor() {
-    this.app = express();
-    this.config();
-    this.routes();
-    this.start();
-  }
+// class SERVER {
+//   constructor() {
+//     this.app = express();
+//     this.config();
+//     this.routes();
+//     this.start();
+//   }
 
-  config() {
-    this.app.use(express.json());
-    this.app.use(morgan('dev'));
-    this.app.use(express.urlencoded({ extended: false }));
-    this.app.use(bodyParser.urlencoded({ extended: false }));
-    this.app.use(bodyParser.json());
-    this.app.use((req, res, next) => {
-      req.header('Cache-Control', 'no-cache');
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
-      );
-      res.header(
-        'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, DELETE'
-      );
-      res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-      next();
-    });
-  }
+//   config() {
+//     this.app.use(express.json());
+//     this.app.use(morgan('dev'));
+//     this.app.use(express.urlencoded({ extended: false }));
+//     this.app.use(bodyParser.urlencoded({ extended: false }));
+//     this.app.use(bodyParser.json());
+//     this.app.use((req, res, next) => {
+//       req.header('Cache-Control', 'no-cache');
+//       res.header('Access-Control-Allow-Origin', '*');
+//       res.header(
+//         'Access-Control-Allow-Headers',
+//         'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+//       );
+//       res.header(
+//         'Access-Control-Allow-Methods',
+//         'GET, POST, OPTIONS, PUT, DELETE'
+//       );
+//       res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//       next();
+//     });
+//   }
 
-  routes() {
-    this.app.get('/', (_, res) => {
-      res.send('Is online :)');
-    });
-    this.app.use(routers);
-    this.app.use(cors());
-    this.app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-  }
+//   routes() {
+//     this.app.get('/', (_, res) => {
+//       res.send('Is online :)');
+//     });
+//     this.app.use(routers);
+//     this.app.use(cors());
+//     this.app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+//   }
 
-  start() {
-    this.app.listen(PORT, () => {
-      console.log('LIVE ON: ', 'http://localhost:' + PORT);
-    });
-    this.app.set('views', path.join(__dirname, 'views'));
-    this.app.set('view engine', 'pug');
-    this.app.use(express.static(path.join(__dirname, 'public')));
-  }
-}
+//   start() {
+//     this.app.listen(PORT, () => {
+//       console.log('LIVE ON: ', 'http://localhost:' + PORT);
+//     });
+//     this.app.set('views', path.join(__dirname, 'views'));
+//     this.app.set('view engine', 'pug');
+//     this.app.use(express.static(path.join(__dirname, 'public')));
+//   }
+// }
 
-const server = new SERVER();
-module.exports = server.app;
+// const server = new SERVER();
+// module.exports = server.app;
+console.log('Hola mundo cruel');
